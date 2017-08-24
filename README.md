f55b1791a507bbb7bf3456b213170cd35f6acdb2
Eric Liu <ericliu314@gmail.com>
2017-08-23 22:11:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Algorithm Helper
+
+Algorithm Helper is an online reference tool for beginner, intermediate or
+advanced programmers to look up sample implementations and notes on a multitude
+of different algorithms, data structures and programming paradigms. It is meant
+to be an comprehensive educational tool. For the algorithms and data 
+structures, we will have introductions explaining their approach and intuition,
+analyze their time and space complexities, and describe real world applications 
+as well as practical implications. Implementations are currently in Java, 
+C/C++, Python and JavaScript.
+
+## Starting up
+
+Get a copy of the project so that you can use Algorithm Helper offline as 
+reference, or for testing purposes. In the terminal:
+
+```
+npm start
+```
+
+For `babel` to transpile ES6 to ES5 code, `gulp-compress` to minify ES5 code,
+and `nodemon` to detect changes and restart server when needed:
+
+```
+npm test
+```
---
+++ server/server.js
@@ -0,0 +1,43 @@
+require('./config/config');
+
+const _ = require('lodash');
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+const fuzzy = require('fuzzy');
+const path = require('path');
+const hbs = require('hbs');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+const partialsPath = path.join(__dirname, '../views/partials');
+const staticPath = path.join(__dirname, '../public');
+
+app.use(express.static(staticPath));
+hbs.registerPartials(partialsPath);
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(session({
+  secret: '<Algorithm Helper Secret>',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
+  }
+}));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index.hbs');
+});
+
+module.exports = {
+  app: app
+};
