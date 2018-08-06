657a46b0069891f63693911091203a07e02059d3
Eric Liu <ericliu314@gmail.com>
2018-08-05 22:37:33 -0400

    Delete unused i18n messages

--- client/utils/i18n.js
+++ client/utils/i18n.js
@@ -1,22 +1,4 @@
 const i18n = {
-  mainArea: {
-    searchPlaceholder: {
-      en: 'Search for a topic...'
-    },
-  },
-  mainAreaCard: {
-    btnText: {
-      en: 'View'
-    }
-  },
-  mainAreaNoSearchResults: {
-    title: {
-      en: 'No results found.'
-    },
-    subtitle: {
-      en: 'Try a different search term.'
-    }
-  },
   contentAreaTableOfContents: {
     title: {
       en: 'Content'
