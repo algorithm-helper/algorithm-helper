9634a8d4bcfb89911f466920e09bbf24f0783d16
Eric Liu <ericliu314@gmail.com>
2018-08-07 03:05:07 -0400

    Fix LoginPage component

--- client/components/LoginPage/LoginPage.js
+++ client/components/LoginPage/LoginPage.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Button, Card, CardBody, Col, Container, Form, FormGroup, Input, InputGroup, Row } from 'reactstrap';
 
-// Components:
-// import Particles from '../Particles/';
-
-// Actions:
 import { resetColorTheme } from '../../actions/ColorThemeActions';
 
 class LoginPage extends React.Component {
@@ -14,85 +11,106 @@ class LoginPage extends React.Component {
       fieldEmail: '',
       fieldPassword: ''
     };
-    this.handleLoginClicked = this.handleLoginClicked.bind(this);
-    this.handleFieldChanged = this.handleFieldChanged.bind(this);
   }
 
   componentWillMount() {
     this.props.dispatch(resetColorTheme());
   }
 
-  handleLoginClicked() {
+  /**
+   * Handles submitting with the given login information.
+   */
+  handleLoginClicked = () => {
     console.log('login clicked');
     console.log(this.state.fieldEmail);
     console.log(this.state.fieldPassword);
-  }
+  };
 
-  handleFieldChanged(e, fieldKey) {
+  /**
+   * Handles the change the field with the given key.
+   *
+   * @param {Event} e
+   * @param {string} fieldKey
+   */
+  handleFieldChanged = (e, fieldKey) => {
     const text = e.target.value.trim();
-    this.setState({
-      [fieldKey]: text
-    });
-  }
+    this.setState({ [fieldKey]: text });
+  };
 
+  /**
+   * Renders the LoginPage component.
+   */
   render() {
     return (
       <div>
-        <div id="container-full-particle-bg">
-
-        </div>
-        <div className="container-fluid container-full">
-          <div className="row">
-            <div className="col-md-2"></div>
-            <div className="col-md-8 col-center">
-              <div className="mx-auto container-login">
-                <div className="card">
-                  <div className="card-body">
-                    <div>
-                      <div className="ah-logo-container">
-                        <img className="ah-logo-small" src="img/logo/algorithm-helper-logo-1000x1000.png" alt="Algorithm Helper Logo" />
-                      </div>
-
-                      <p className="h4 text-center py-4">Login To Your Account</p>
-                      <div className="md-form">
-                        <i className="fa fa-envelope prefix grey-text"></i>
-                        <input
-                          type="email"
-                          id="materialFormCardEmailEx"
-                          className="form-control"
-                          autoComplete="off"
-                          onChange={e => this.handleFieldChanged(e, 'fieldEmail')}
-                        />
-                        <label htmlFor="materialFormCardEmailEx" className="font-weight-light">Email</label>
-                      </div>
-                      <div className="md-form">
-                        <i className="fa fa-lock prefix grey-text"></i>
-                        <input
-                          type="password"
-                          id="materialFormCardPasswordEx"
-                          className="form-control"
-                          autoComplete="off"
-                          onChange={e => this.handleFieldChanged(e, 'fieldPassword')}
-                        />
-                        <label htmlFor="materialFormCardPasswordEx" className="font-weight-light">Password</label>
-                      </div>
-                      <div className="text-center py-4 mt-3">
-                        <button
-                          className="btn btn-deep-purple"
-                          type="button"
-                          onClick={this.handleLoginClicked}
-                        >
-                          Login
-                        </button>
-                      </div>
+        <Container className="container-full" fluid>
+          <Row>
+            <Col md="2"/>
+            <Col md="8">
+              <div className="mx-auto login-page-card-container">
+                <Card className="login-page-card">
+                  <CardBody>
+                    <div className="login-page-logo-container">
+                      <img
+                        className="login-page-logo"
+                        src="img/logo/logo-dark.png"
+                        alt="Algorithmica Logo"
+                      />
                     </div>
-                  </div>
-                </div>
+
+                    <div className="login-page-title">
+                      Login To Your Account
+                    </div>
+
+                    <Form className="login-page-form">
+                      <FormGroup>
+                        <i className="fa fa-envelope prefix grey-text"/>
+                        <label htmlFor="login-page-email" className="login-page-label">
+                          Email
+                        </label>
+                        <InputGroup>
+                          <Input
+                            className="login-page-input"
+                            type="email"
+                            id="login-page-email"
+                            autoComplete="off"
+                            onChange={e => this.handleFieldChanged(e, 'fieldEmail')}
+                          />
+                        </InputGroup>
+                      </FormGroup>
+
+                      <FormGroup>
+                        <i className="fa fa-lock prefix grey-text"/>
+                        <label htmlFor="login-page-password" className="login-page-label">
+                          Password
+                        </label>
+                        <InputGroup>
+                          <Input
+                            className="login-page-input"
+                            type="password"
+                            id="login-page-password"
+                            autoComplete="off"
+                            onChange={e => this.handleFieldChanged(e, 'fieldPassword')}
+                          />
+                        </InputGroup>
+                      </FormGroup>
+                    </Form>
+
+                    <div className="login-page-btn-login-container">
+                      <Button
+                        className="login-page-btn-login"
+                        color="secondary"
+                        onClick={this.handleLoginClicked}>
+                        Login
+                      </Button>
+                    </div>
+                  </CardBody>
+                </Card>
               </div>
-            </div>
-            <div className="col-md-2"></div>
-          </div>
-        </div>
+            </Col>
+            <Col md="2"/>
+          </Row>
+        </Container>
       </div>
     );
   }
@@ -103,5 +121,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(LoginPage);
-
-//           <Particles />
--- client/components/SignUpPage/SignUpPage.js
+++ client/components/SignUpPage/SignUpPage.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-// Components:
-// import Particles from '../Particles/';
+import { Button, Col, Container, Row } from 'reactstrap';
 
 // Actions:
 import { resetColorTheme } from '../../actions/ColorThemeActions';
-
-// Utils:
-// import { signupValidation } from '../utils/validation.js';
 
 class SignUpPage extends React.Component {
   constructor(props) {
@@ -44,72 +39,82 @@ class SignUpPage extends React.Component {
   render() {
     return (
       <div>
-        <div id="container-full-particle-bg">
-
-        </div>
-        <div className="container-fluid container-full">
-          <div className="row">
-            <div className="col-md-2"></div>
-            <div className="col-md-8 col-center">
+        <Container className="container-full" fluid>
+          <Row>
+            <Col md="2"/>
+            <Col className="col-center" md="8">
               <div className="mx-auto container-sign-up">
                 <div className="card">
                   <div className="card-body">
                     <div>
                       <div className="ah-logo-container">
-                        <img className="ah-logo-small" src="img/logo/algorithm-helper-logo-1000x1000.png" alt="Algorithm Helper Logo" />
+                        <img
+                          className="ah-logo-small"
+                          src="img/logo/algorithm-helper-logo-1000x1000.png"
+                          alt="Algorithm Helper Logo"
+                          style={{
+                            width: '50%',
+                            textAlign: 'center',
+                          }}/>
                       </div>
 
-                      <p className="h4 text-center py-4">Sign Up For An Account</p>
+                      <p className="h4 text-center py-4">
+                        Sign Up For An Account
+                      </p>
                       <div className="md-form">
-                        <i className="fa fa-user prefix grey-text"></i>
+                        <i className="fa fa-user prefix grey-text"/>
+                        <label htmlFor="sign-up-page-full-name" className="font-weight-light">
+                          Full Name
+                        </label>
                         <input
                           type="text"
-                          id="materialFormCardNameEx"
+                          id="sign-up-page-full-name"
                           className="form-control"
                           autoComplete="off"
                           onChange={e => this.handleFieldChanged(e, 'fieldFullName')}
                         />
-                        <label htmlFor="materialFormCardNameEx" className="font-weight-light">Full Name</label>
                       </div>
                       <div className="md-form">
-                        <i className="fa fa-envelope prefix grey-text"></i>
+                        <i className="fa fa-envelope prefix grey-text"/>
+                        <label htmlFor="sign-up-page-email" className="font-weight-light">
+                          Email
+                        </label>
                         <input
                           type="email"
-                          id="materialFormCardEmailEx"
+                          id="sign-up-page-email"
                           className="form-control"
                           autoComplete="off"
                           onChange={e => this.handleFieldChanged(e, 'fieldEmail')}
                         />
-                        <label htmlFor="materialFormCardEmailEx" className="font-weight-light">Email</label>
                       </div>
                       <div className="md-form">
-                        <i className="fa fa-lock prefix grey-text"></i>
+                        <i className="fa fa-lock prefix grey-text"/>
+                        <label htmlFor="sign-up-page-password" className="font-weight-light">
+                          Password
+                        </label>
                         <input
                           type="password"
-                          id="materialFormCardPasswordEx"
+                          id="sign-up-page-password"
                           className="form-control"
                           autoComplete="off"
                           onChange={e => this.handleFieldChanged(e, 'fieldPassword')}
                         />
-                        <label htmlFor="materialFormCardPasswordEx" className="font-weight-light">Password</label>
                       </div>
                       <div className="text-center py-4 mt-3">
-                        <button
-                          className="btn btn-deep-purple"
-                          type="button"
-                          onClick={this.handleSignupClicked}
-                        >
+                        <Button
+                          className="sign-up-page-btn-register"
+                          onClick={this.handleSignupClicked}>
                           Register
-                        </button>
+                        </Button>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col-md-2"></div>
-          </div>
-        </div>
+            </Col>
+            <Col md="2"/>
+          </Row>
+        </Container>
       </div>
     );
   }
