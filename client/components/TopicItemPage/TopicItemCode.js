a8d97b202dad8697d0069d08ded28b6d156704d6
Eric Liu <ericliu314@gmail.com>
2018-08-13 01:43:36 -0400

    Update topicIndex.json

--- client/components/TopicItemPage/TopicItemCode.js
+++ client/components/TopicItemPage/TopicItemCode.js
@@ -15,95 +15,11 @@ class TopicItemCode extends React.Component {
     this.state = {
       codeContent: [],
       value: 0,
-      loading: false,
     };
   }
 
   componentWillMount() {
     this.props.contentLoaded();
-
-    const sample1 = `// Sample 1 - Imports the Google Cloud client library
-const Storage = require('@google-cloud/storage');
-
-// Creates a client
-const storage = new Storage();
-
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// const bucketName = 'Name of a bucket, e.g. my-bucket';
-// const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
-
-// Uploads a local file to the bucket
-storage
-.bucket(bucketName)
-.upload(filename)
-.then(() => {
-  console.log('filename uploaded to bucket name');
-})
-.catch(err => {
-  console.error('ERROR:', err);
-});`;
-
-    const sample2 = `// Sample 2 - Imports the Google Cloud client library
-const Storage = require('@google-cloud/storage');
-
-// Creates a client
-const storage = new Storage();
-
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// const bucketName = 'Name of a bucket, e.g. my-bucket';
-// const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
-
-// Uploads a local file to the bucket
-storage
-.bucket(bucketName)
-.upload(filename)
-.then(() => {
-  console.log('filename uploaded to bucket name');
-})
-.catch(err => {
-  console.error('ERROR:', err);
-});`;
-
-    const sample3 = `// Sample 3 - Imports the Google Cloud client library
-const Storage = require('@google-cloud/storage');
-
-// Creates a client
-const storage = new Storage();
-
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// const bucketName = 'Name of a bucket, e.g. my-bucket';
-// const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
-
-// Uploads a local file to the bucket
-storage
-.bucket(bucketName)
-.upload(filename)
-.then(() => {
-  console.log('filename uploaded to bucket name');
-})
-.catch(err => {
-  console.error('ERROR:', err);
-});`;
-
-    this.setState({
-      loading: true,
-      codeContent: [
-        sample1,
-        sample2,
-        sample3
-      ]
-    });
-
-    setTimeout(() => {
-      this.props.contentLoaded();
-      this.setState({ loading: false });
-    }, 500);
   }
 
   /**
@@ -116,7 +32,7 @@ storage
           defaultActiveKey="0"
           renderTabBar={() => <ScrollableInkTabBar/>}
           renderTabContent={() => <TabContent/>}>
-          <TabPane tab="Java" key="0">
+          <TabPane tab="Java (HashMapLinearProbing)" key="0">
             Test content here
           </TabPane>
           <TabPane tab="Python" key="1">
