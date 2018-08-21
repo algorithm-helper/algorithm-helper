920821ffa20c769387b8b8918c343e5262a3edf5
Eric Liu <ericliu314@gmail.com>
2018-08-21 01:49:53 -0400

    Use bcrypt for user password hashing

--- server/mongo/models/User.js
+++ server/mongo/models/User.js
@@ -1,7 +1,8 @@
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const validator = require('validator');
-const jwt = require('jsonwebtoken');
 
 const MIN_FULL_NAME_LENGTH = 1;
 const MIN_EMAIL_LENGTH = 1;
@@ -115,6 +116,21 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.access': 'auth',
   });
 }
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 const User = mongoose.model('User', UserSchema);
 
