fd37e059be7075a724b47847db404d95ec7f6e29
Eric Liu <ericliu314@gmail.com>
2018-08-23 14:42:09 -0400

    Fix client webpack config

--- webpack/webpack-client.config.js
+++ webpack/webpack-client.config.js
@@ -1,11 +1,27 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|mjs)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      }
-    ]
-  }
+var path = require('path');
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const outputDir = isProduction ? 'prod' : 'dev';
+
+  return {
+    context: path.resolve(__dirname, '..', 'src', 'client'),
+    entry: './app.js',
+    output: {
+      filename: 'bundle-client.js',
+      path: path.resolve(__dirname, '..', 'dist', outputDir),
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    resolve: {
+        modules: [
+          path.resolve(__dirname, '..', 'src', 'client'),
+          path.resolve(__dirname, '..', 'node_modules'),
+        ],
+    },
+    stats: {
+      warnings: false
+    },
+  };
 };
